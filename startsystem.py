3673bb88b82e6bb4179b3c50425fbfcbc7a14bf5
JPDevOz <101525753+JoaoPedroDevOz@users.noreply.github.com>
2022-11-29 20:08:49 -0300

    Initial commit

---
+++ startsystem.py
@@ -0,0 +1,18 @@
+from main import  *
+
+arq = 'Palavras.txt'
+ask = ''
+
+if archExist(arq):
+    print()
+else:
+    createarch(arq)
+
+while ask != "N":
+    register(arq, str(input("Palavra: ")))
+    ask = str(input('Do you want to continue? [YN] ')).upper()
+    while ask not in 'YN':
+        ask = str(input('Do you want to continue? [YN] ')).upper()
+        continue
+    if ask == 'N':
+        break
